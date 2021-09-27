3c3e6630e6b248e5a41997ffa4df1af61bfad7e9
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-09-27 06:30:41 -0400

    commit 054403946 on 20210927

---
+++ ma.bash
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+# Copyright 2019-2021 (c) all rights reserved
+# by BuildAPKs https://buildapks.github.io
+#####################################################################
+set -Eeuo pipefail
+shopt -s nullglob globstar
+. "$RDR"/scripts/bash/shlibs/trap.bash 210 211 212
+cd "$JDR"
+# _AT_ username/repository commit
+_AT_ AndroidAalto/BookingRoom 93679e03736f68a2d9416d1d2c9f2e8ae6b71441
+_AT_ AndroidAalto/SoundFuse 972d566b10fa36613327b205dfad34f8c3ab02db
+_AT_ Arjun93/Bongos-Maestro 1ea65240bf6915c6bac5d60effab23e1b190f6e0
+_AT_ Bubu/athg2sms bb644440e228b4edc14a76fd9963ed5fa6f6883b
+_AT_ abramhindle/AndroidTableEditor ea5d0405c3cb7d5e4aa8c7ba87a590829e33e97d
+_AT_ adriangl/metromadrid-android bfbb78970ecc1a44519af00fd984c96bdb20540c
+_AT_ afairley/Tesla-Table 9b64cbb7687dde663690c2c2c7621d2175c9429a
+_AT_ agehua/achartengine 2a318ee8a4a9fa809d9ab11a3a22b73ad0f3d5e6
+_AT_ ahmed9dz/flashlight 04287aa86097049612c8d5168b99f85329914d4d
+_AT_ alexandraHospital/1024Histoires fa3682bada9841ad6bf9eb89f508941aa301a7e9
+_AT_ amsitlab/termuxlauncher b543e1d0e20cb36ce0c801ad3cbee0d27c055882
+_AT_ arsho/drumpad_android_application 9c5856caa334feab7b5c63fc904f9fcfacb2a224
+# _AT_ andviane/moon d841e446e05e506ef546add78a6dacd635740ebc
+# _AT_ andviane/prism 8c8813ebd48fa1aee31ed72431665db493f885d3
+_AT_ aosp-mirror/platform_packages_apps_htmlviewer 0e521ae9aa2c8b1b3d30870ec9662affef4b61a7
+_AT_ aosp-mirror/platform_packages_apps_music 922216b0b0a57e56663dd480f908589cb8e5159d
+_AT_ ar-/sendreduced 2d7498493ad044abd206f7f40f1cc97f5768dfe8
+_AT_ axxapy/android.tools.app-ops 00c4bb13569e63c1c3a02f28894285d355290b64
+_AT_ badel2/termux-api dd22e5a8da02526ed5c965206952c24d79ac49a2
+_AT_ bhaskar-c/AndroidBluetoothMidiController 717f8cf387452e3f65c12294051b1eae43842f68
+# _AT_ billthefarmer/accordion b7959df0da6af4202e40c3de527b55d19efc2e0a
+_AT_ CyanBlue/TipCalculator d9b67cabc39df37f3021b7c706c9126274133762
+_AT_ citizenterminal/android-palindrome-tipper 3ef1f6434b399b1b6ae69d6604b6aec247313d0c
+_AT_ CircuitFreakCoder/AndroidDetectInternetConnection aee34f11cd8dc77acf287bd4f68d258551dc5138
+_AT_ folee/REC 4961b7ab082655b25bc7c436df3d437b48b47a89
+_AT_ google/ringdroid 955039d67252ea7aea5a36933e8ee30110f5ec92
+_AT_ greg-pe/platform_packages_apps_SoundRecorder 9c75bb028c2761f47ae0817915002c433fa7ea8d
+_AT_ helloworld1/BurnInDaydream e6c3013a4534b8c4da2d6948f1abbf16db1cd0fd
+_AT_ helloworld1/AnyUnlock 69613d4b97f9bbcc85cb62e95061250e510650cd
+_AT_ hguochen/FULLCam 25e710a44e5024f267ef9271be5d76af20300fa0
+_AT_ hzeller/precise-pitch 851ea5bd504f842138b09f3ddfc511a6eba1d76f
+_AT_ jblough/bible-android 48461d1a30422758b619bba31288c38543cd8fec
+_AT_ jfifield/note-trainer ac59a3640561c19332b6b9a4003653b05292abb9
+_AT_ josepedro/ChordsDetector 0c2c12ca1499c7e64d737741c25b8f1490c7c926
+_AT_ lrq3000/hexiano 2c7012599f1aa00939d51340a206774503061057
+_AT_ matthewrkula/WifiGamepad f37e096693d17c4e32933a2db3d64fb31c0868a6
+_AT_ meefik/wshell 7748b6b67231a1e9e17f51d0aded7b142246ca1b
+_AT_ mobilepearls/com.mobilepearls.appsharer d22b2e7ce2f9cda5018fe913b9f018512c3e7be8
+_AT_ nekromant/NetTTS d559e6ad2b2d9e98efdb8889f78f4910e50b9960
+_AT_ nemik/sms2blog-mobile-server c8c91cc4bfbac3f3cd2629393c1a0bf35161c03a
+_AT_ nidhi1608/TipCalculator fedac196588b40c201e2d7dbf1c88ff12562ca3b
+_AT_ nolanlawson/ChordReaderRoot 8dafebfbdfaf8784109dd16a899553bcffbd05ae
+_AT_ phora/AndroPTPB 75a794aba7a59e610b3973682c8a2d3bc77efed1
+_AT_ phora/AndroTSH 5b236d6b35e4203496c6b052d83c27aab4757f0b
+_AT_ pplutap/metal_field_detector c959a37a621d4e236cb60d6a77fbe3657b2e40ef
+_AT_ pyricau/buloid 01255e347b02f24bef79afdc641dd75aaa4f53b3
+_AT_ rath/android-calculator 0b29562b5b4fea96c10723cec663016702ee6b04
+_AT_ retrohacker/CS202-Final-Project a9825c39fb10b54e0bc014d5e145a3c6fb703cd9
+# _AT_ rrelmy/BatteryLevel b24f788d64771506db622b9d6f9837be6d78db05
+_AT_ quaap/ComputationalDemonology e0dbcbb5aed90f427bf1a24d5bf9a30520b7b5db
+_AT_ quen/humiditytracker d2e5d74a169982cab3b96d3968472f0b77d31c53
+_AT_ SaketSrivastav/SimpleInterestCalculator 1fcaa865765df176dcca8cf44d217bc1f32adab0
+_AT_ shishir2588/PianoApp d581a070411b1042bec11776d35068ed67ed6ed9
+_AT_ stellaraccident/speakpad dd0ce89e28b5a0a1c8102b0f80b94a925323dec9
+_AT_ sungkim23/CoolCalc 8f5772a8fafa711b060847983c2949554030f3c7
+_AT_ tobykurien/GoogleApps 318bdd857732f652d2fc95592c2a5a3037882784
+_AT_ termux/TermuxAm 1620e7c88bb1c120afa9301a423374b43637dcf1
+_AT_ termux/termux-styling 2fb263e596d5c61549b6ef7e2dab31addde23059
+_AT_ termux/termux-failsafe 7f175afc1ec25baf77b1e2214c667abb947572a1
+_AT_ timb0/CrystalBall 139bcc424feb344d72e82219845a1974aa119b94
+_AT_ valerio-bozzolan/AcrylicPaint 03e3629979cd2de0f389f44e12f51700a6ceac0d
+_AT_ valerio-bozzolan/gilgamesh cb044b24171378ef3a28344d7371496419223be0
+# _AT_ vxhex/SmsBomber 66407d71a8fe241678a904d6f82f144cca479a1a
+_AT_ winterbe/android-moneysheet e00491ddebc0a096ec1b40a179778877d6ca2fe5
+# _AT_ wrr/speech_trainer 4fdd845f0a7ec0e9c7aef6f1290ffaf8999878f2
+# rm -rf wrr/speech_trainer/wrr-speech_trainer-4fdd845/app_tests/
+_AT_ yetanotherchris/chordboard 886601c5b3a0d0ebda762107c1070210da00403d
+# BuildAPKs/buildAPKs.apps ma.bash OEF
